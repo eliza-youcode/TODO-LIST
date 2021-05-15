14307d1ff438741f72ebd2f79754b0a7c33fbac3
Eliza Śpionek <eliza.spionek@gmail.com>
2021-05-15 19:23:28 +0200

    add function newTaskContent

--- js/script.js
+++ js/script.js
@@ -16,16 +16,37 @@
 
         for (const task of tasks) {
             htmlString += `
-    <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+    <li
+    ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+    >
     ${task.content}
     </li>
     `;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+
+    };
 
     const init = () => {
         render();
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", (evenet) => {
+            evenet.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+            if (newTaskContent === "") {
+                return;
+            }
+           addNewTask(newTaskContent);
+        });
 
     };
 
