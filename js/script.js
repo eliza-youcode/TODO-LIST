fec7b3c53ad01cf194af0c50c4389e4368b0450b
Eliza Śpionek <eliza.spionek@gmail.com>
2021-05-15 19:41:04 +0200

    removeTask added

--- js/script.js
+++ js/script.js
@@ -10,6 +10,17 @@
         },
 
     ];
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
 
     const render = () => {
         let htmlString = "";
@@ -19,34 +30,40 @@
     <li
     ${task.done ? " style=\"text-decoration: line-through\"" : ""}
     >
+    <button class="js-remove">usuń</button> 
     ${task.content}
     </li>
     `;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
-        render();
+        const removeButton = document.querySelectorAll(".js-remove");
 
+
+        removeButton.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+    };
+   
+
+    const onFormSubmit = (evenet) => {
+        evenet.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent);
     };
 
     const init = () => {
         render();
         const form = document.querySelector(".js-form");
 
-        form.addEventListener("submit", (evenet) => {
-            evenet.preventDefault();
+        form.addEventListener("submit", onFormSubmit);
 
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
-            if (newTaskContent === "") {
-                return;
-            }
-           addNewTask(newTaskContent);
-        });
 
     };
 
